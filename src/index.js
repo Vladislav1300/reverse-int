9d028c70db9266d3d608889defcc90e1a46c8290
Vladislav1300 <askerko1997@gmail.com>
2022-08-01 13:35:29 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(n).split('')
+    if(arr[0] === '-') {
+      return arr.splice(1, arr.length).reverse().join('');
+    }
+    else return arr.reverse().join('');
 }
